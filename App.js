0609ed983e240c3c6302d98e36099d475362496f
thirawutp <thirawut.p@appman.co.th>
2017-10-28 00:18:09 +0700

    set up push notification

--- App.js
+++ App.js
@@ -1,5 +1,12 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
 import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
+var PushNotification = require('react-native-push-notification');
 
 const instructions = Platform.select({
     ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -8,6 +15,32 @@ const instructions = Platform.select({
 });
 
 export default class App extends Component {
+    componentDidMount() {
+        PushNotification.configure({
+            onRegister: function(token) {
+                console.log('TOKEN:', token);
+            },
+            onNotification: function(notification) {
+                console.log('NOTIFICATION:', notification);
+                const { id } = notification;
+                const title = notification['gcm.notification.title'];
+                const message = notification['gcm.notification.body'];
+                PushNotification.localNotification({
+                    title,
+                    message,
+                    actions: '["Yes", "No"]'
+                });
+            },
+            permissions: {
+                alert: true,
+                badge: true,
+                sound: true
+            },
+            popInitialNotification: true,
+            // senderID: '949834818940', //only android
+            requestPermissions: true
+        });
+    }
     render() {
         return (
             <View style={styles.container}>
